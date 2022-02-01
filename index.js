ea40e6e1666959b8a0e10a422743a73bce317ad2
Brandon <blor15@hotmail.com>
2022-01-31 19:32:30 -0700

    starting point for todo app

---
+++ index.js
@@ -0,0 +1,22 @@
+//Creating an array of objects which will store every input of the user inside the array
+const todoObjectList = [];
+
+/* Created a class called Todo_Class which uses the <ul> element as the parameter of the class and then set up methods for adding the user input to todoObjectList, changing the done or undone status of <li> element, deleting elements and displaing the list on screen */
+class Todo_Class {
+    constructor(item) {
+        this.ulElement = item;
+    }
+
+    add() {
+    }
+
+    done_undone(x) {
+    }
+
+    deleteElement(z) {
+    }
+
+    display() {
+    }
+}
+
